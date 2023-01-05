f071368ece39c38a691239cf548a4f98a52fd1cc
Daniel Russell Alexander <92579202+alexander-d-r@users.noreply.github.com>
2023-01-05 14:25:36 +0700

    funni yoke

---
+++ sources.js
@@ -0,0 +1,7 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+// bra1nz_2of2_1jh34b21}
